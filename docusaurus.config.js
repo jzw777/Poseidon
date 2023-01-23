1bd5948c47609bbfe9f571a7a27e9fae4b8f9b88
jiangzw09 <jiangzw09@tkhealthcare.com>
2023-01-23 11:26:36 +0800

    docker 文档

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -25,6 +25,8 @@ const config = {
   // Even if you don't use internalization, you can use this field to set useful
   // metadata like html lang. For example, if your site is Chinese, you may want
   // to replace "en" with "zh-Hans".
+  trailingSlash: false,
+
   i18n: {
     defaultLocale: 'zh-Hans',
     locales: ['zh-Hans'],
